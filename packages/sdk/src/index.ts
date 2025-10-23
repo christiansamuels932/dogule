d43ecd8a9c697cc7ee8791e4326149b3098efc82
christiansamuels932 <christiansamuels932@gmail.com>
2025-10-23 14:05:23 +0200

    feat(sdk): add base url + default /api and wire web bootstrap

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -2,6 +2,7 @@ import { createRequestClient } from "./request.js";
 import { createResources } from "./resources.js";
 export * from "./errors.js";
 export * from "./types.js";
+export { setBaseUrl } from "./request.js";
 
 export function createDoguleSDK(config: Parameters<typeof createRequestClient>[0]) {
   const requestClient = createRequestClient(config);
